9e6cbea2c4722e71ebccf9cf808536a07b295238
Vincent Molleno <vincent.molleno.777@gmail.com>
2026-06-07 19:25:28 +0800

    bumped version for Wattpad plugin

--- plugins/english/WattpadPlugin.ts
+++ plugins/english/WattpadPlugin.ts
@@ -8,7 +8,7 @@ class WattpadPlugin implements Plugin.PluginBase {
   name = 'Wattpad';
   icon = 'plugins/english/wattpad/icon.png';
   site = 'https://www.wattpad.com';
-  version = '1.1.0';
+  version = '1.1.1';
 
   async popularNovels(
     pageNo: number,
